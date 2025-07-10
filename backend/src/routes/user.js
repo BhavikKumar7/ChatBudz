8befc872fa90c78ec2b162b58dd2f87e011e2a6f
BhavikKumar7 <bhavikrajputrr2004@gmail.com>
2025-07-11 02:39:07 +0530

    11/07

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.js";
+import { 
+    getRecommendedUsers, 
+    getMyFriends, 
+    sendFriendRequest, 
+    acceptFriendRequest, 
+    getFriendRequest,
+    getOutgoingFriendReqs
+} from "../controllers/user.js";
 
 const router = express.Router();
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+router.get("/friend-requests", getFriendRequest);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
