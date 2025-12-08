dbdb7db9862d8a7744b32b587f28978b112891ba
BhavikKumar7 <bhavikrajputrr2004@gmail.com>
2025-12-08 23:14:03 +0530

    login and logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,27 +2,27 @@ import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import {ENV} from "../lib/env.js";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
-    const {fullName, email, password } = req.body;
+    const { fullName, email, password } = req.body;
 
     try {
-        if(!fullName || !email || !password){
+        if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        if(password.length < 6){
+        if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters long" });
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
+        if (!emailRegex.test(email)) {
             return res.status(400).json({ message: "Invalid email format" });
         }
 
-        const user = await User.findOne({email});
-        if(user){
+        const user = await User.findOne({ email });
+        if (user) {
             return res.status(400).json({ message: "Email already exists" });
         }
 
@@ -35,15 +35,15 @@ export const signup = async (req, res) => {
             password: hashedPassword
         });
 
-        if(newUser){
+        if (newUser) {
             const savedUser = await newUser.save();
             generateToken(savedUser._id, res)
 
             res.status(201).json({
-                _id:newUser._id,
-                fullName:newUser.fullName,
-                email:newUser.email,
-                profilePic:newUser.profilePic,
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
             });
 
             try {
@@ -52,15 +52,48 @@ export const signup = async (req, res) => {
                 console.error("Failed to send welcome email: ", error);
             }
 
-        }else{
+        } else {
             res.status(400).json({ message: "Invalid user data" });
         }
     } catch (error) {
         console.log("Error in signup controller: ", error);
-        res.status(500).json({ message: "Internal Server Error "});
+        res.status(500).json({ message: "Internal Server Error " });
     }
 };
 
 export const login = async (req, res) => {
-    
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are Required" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.error("Error in login controller: ", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie("jwt", "", {
+        maxAge: 0,
+    });
+
+    return res.status(200).json({ message: "Logged out successfully" });
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,10 @@
 import express from "express";
-import { login, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-
-router.get("/logout", (req, res) => {
-    res.send("Logout endpoint");
-});
+router.post("/logout", logout);
 
 export default router;
